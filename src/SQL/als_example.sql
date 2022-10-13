f4d2e58994f6b8fdc549eff807f5fe768b8651ae
Song <xsm7f@umsystem.edu>
2022-10-13 12:36:29 -0500

    upload data i/o and prepartion scripts

---
+++ src/SQL/als_example.sql
@@ -0,0 +1,6 @@
+/*
+# Copyright (c) 2021-2025 University of Missouri                   
+# Author: Xing Song, xsm7f@umsystem.edu                            
+# File: als_example.sql                                                 
+# Description: This script is for generating the example ALS cohort dataset for in-class exercise
+*/
